77a35697f1f8dabbf6ab2e85a1f5fcaa75221dac
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-06-08 16:06:06 -0300

    implementar função de aplicação de desconto dos livros

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,17 @@ async function getBuscarLivrosDaAPI() {
   const res = await fetch(endpointDaAPI);
   livros = await res.json();
   console.table(livros);
-  exibirlivro(livros);
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirlivro(livrosComDesconto);
+}
+function aplicarDesconto (livros) {
+  const desconto = 0.3;
+  livrosComDesconto = livros.map(livro => {
+    const precoComDesconto = livro.preco - (livro.preco * desconto);
+    return {...livro, preco: precoComDesconto.toFixed(2)
+    };
+  })
+  return livrosComDesconto;
 }
 function exibirlivro(listaDeLivros) {
   listaDeLivros.forEach(livro => {
